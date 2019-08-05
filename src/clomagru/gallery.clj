21d879fe8d3adcc61d53703665334d962c9785fc
Colin PIREYRE <cpireyre@student.42.fr>
2019-08-05 09:56:53 +0200

    Protecting against SQL injection (oops) by checking if req is a UUID.

--- src/clomagru/gallery.clj
+++ src/clomagru/gallery.clj
@@ -1,13 +1,12 @@
 (ns clomagru.gallery
   (:require [clomagru.db :as db]
             [ring.util.http-response :refer :all])
-  (:import java.io.ByteArrayInputStream))
-
 ;;  "files" below is the name of the database table...
 ;;  Database internals are leaking.
+
 (defn get-image [uuid]
-  (if-let [{:keys [files/type files/data]} (first (db/get-image uuid))]
-    (-> (ByteArrayInputStream. data)
-        (ok)
-        (content-type type))
-    (str "didn't find it")))
+  (if (uuid? uuid)
+    (if-let [picc (first (db/get-image uuid))]
+      (clojure.pprint/pprint (:files/data picc))
+      (not-found))
+    (str "All pics are referred to by UUIDs around here.")))
