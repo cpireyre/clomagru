7944e2f3dbaf7147966033eee8135899be87f33b
Colin PIREYRE <cpireyre@student.42.fr>
2019-08-25 15:24:23 +0200

    Upgrading to 1.10.1 for the new error messages.

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/clojurescript "1.10.520"]
                  [org.clojure/test.check "0.10.0"]
                  [ring "1.7.1"]
