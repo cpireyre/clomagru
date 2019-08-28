fdb12d3e4b10b9b219b8ecfe05da2d01a108d7b9
cpireyre <cpireyre@student.42.fr>
2019-08-28 18:49:43 +0200

    Changed send-mail! logging.

--- src/clomagru/email.clj
+++ src/clomagru/email.clj
@@ -4,9 +4,9 @@
             [clomagru.log :as log]))
 
 (defn send-mail! [recipient subject body]
-  (log/timelog-stdin "Sending e-mail to" recipient)
-  (p/send-message key/smtp-params
-                  {:from    "clomagru42@gmail.com"
-                   :to      recipient
-                   :subject subject
-                   :body    body}))
+  (let [mail-return-code (p/send-message key/smtp-params
+                                         {:from    "clomagru42@gmail.com"
+                                          :to      recipient
+                                          :subject subject
+                                          :body    body})]
+    (log/timelog-stdin "Sending e-mail to" recipient (str ": " mail-return-code))))
