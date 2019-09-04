bd7b878be89d66818a91b1b4520f13632d74c1e9
Colin PIREYRE <cpireyre@student.42.fr>
2019-09-04 11:07:46 +0200

    comment-picc route now responds with a 201 and a Clojure map describing the data.

--- src/clomagru/comment.clj
+++ src/clomagru/comment.clj
@@ -5,7 +5,8 @@
             [clomagru.db :as db]
             [clomagru.gallery :refer [string-uuid?]]
             [clomagru.users :refer [valid-username?]]
-            [ring.util.response :as response]))
+            [ring.util.response :as response]
+            [clomagru.gallery :as gallery]))
 
 (defn good-comment? [poster-uuid pic-uuid comment]
   "Checks that UUIDs were passed, and that these UUIDs refer
@@ -22,20 +23,23 @@
   (if (good-comment? poster-uuid pic-uuid comment)
     (do
       (log/timelog-stdin poster-uuid "commented on" pic-uuid)
-      (sql/insert! ds :comments {:poster_uuid poster-uuid
-                                 :created_at (System/currentTimeMillis)
-                                 :pic_uuid    pic-uuid
-                                 :comment     comment}))
+      (let [comment-data {:poster_uuid poster-uuid
+                          :created_at (System/currentTimeMillis)
+                          :pic_uuid    pic-uuid
+                          :comment     comment}]
+        (sql/insert! ds :comments comment-data)
+        (-> (gallery/transit-str comment-data)
+            (response/response)
+            (response/status 201)
+            (response/content-type "text/plain"))))
     :bad-comment))
 
 (defn post-comment! [req] ;; maybe just asyc this.
   (if-let [user-uuid (get-in req [:session :uuid])]
-    (do
-      (save-comment!
-        (get-in req [:session :uuid])
-        (get-in req [:params :pic-uuid])
-        (get-in req [:params :comment]))
-      (response/redirect "/")) ;; this doesn't return a 201 and doesn't really redirect where I would like.
+    (str (save-comment!
+      (get-in req [:session :uuid])
+      (get-in req [:params :pic-uuid])
+      (get-in req [:params :comment]))) ;; this doesn't return a 201 and doesn't really redirect where I would like.
     (-> (response/response "Unauthorized")
         (response/status 401)
         (response/content-type "text/plain"))))
