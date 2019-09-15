46b32d1ffa166af477d3f74d5d2de45001165ae4
Colin PIREYRE <cpireyre@student.42.fr>
2019-09-15 12:55:12 +0200

    WIP: Password reset feature.

--- src/clomagru/forgot.clj
+++ src/clomagru/forgot.clj
@@ -27,6 +27,16 @@
     (when (seq ret)
       (first ret))))
 
+(defn get-token
+  "Takes a token string and queries the datasource for the corresponding map.
+  Returns nil on failure to find token."
+  [ds tok]
+  (let [ret (sql/query ds
+                       ["select * from pwtokens where token = ?"
+                        tok])]
+    (when (seq ret)
+      (first ret))))
+
 (defn make-token
   "Takes a user UUID and returns a token map
   suitable for insertion in database."
@@ -43,11 +53,24 @@
   "Takes a user map, generates a token, sends the e-mail containing
   the password reset link."
   [user]
-  (let [id      (:accounts/id user)
-        tok     (make-token id)
-        tok-str (:token tok)
-        email   (:accounts/email user)]
+  (let [id       (:accounts/id user)
+        username (:accounts/username user)
+        tok      (make-token id)
+        tok-str  (:token tok)
+        email    (:accounts/email user)]
     (insert-token! ds tok)
     (send-forgot-email! email username tok-str)))
 
-;; TODO: check error pre conditions, verify token, reset password.
+;; TODO: check error pre conditions, verify token, reset password, add handler.
+
+(defn confirm-token!
+  "Takes a datasource and token map, finds the corresponding user,
+  resets their password, deletes the token from the database."
+  [ds tok encrypted-new-pw]
+  (timelog-stdin "processing password reset token" tok)
+  (sql/update!
+    ds
+    :accounts
+    {:password encrypted-new-pw}
+    {:id (:pwtokens/owner tok)})
+  (sql/delete! ds :pwtokens {:id (:pwtokens/id tok)}))
