367751cffaa19225f05a13b3a41864f65a989ce2
Colin PIREYRE <cpireyre@student.42.fr>
2019-09-15 12:11:23 +0200

    WIP: password reset feature.

---
+++ src/clomagru/forgot.clj
@@ -0,0 +1,53 @@
+(ns clomagru.forgot
+  (:require [clomagru.email :as email]
+            [clomagru.initdb :refer [ds]]
+            [clomagru.log :refer [timelog-stdin]]
+            [clomagru.db :as db]
+            [clomagru.users :refer [example-pw]]
+            [next.jdbc.sql :as sql]))
+
+(defn forgot-password-mail [username token]
+  (str "Henlo, " username ".\n"
+       "If you've forgotten your Clomagru password, "
+       "you may reset it here:\n"
+       "TBD " token))
+
+(defn send-forgot-email! [email username token]
+  (email/send-mail! email
+                    "Clomagru: forgot your password?"
+                    (forgot-password-mail username token)))
+
+(defn get-user-info 
+  "Takes the username, queries the datasource, returns e-mail and uuid.
+  Returns nil if no such user was found."
+  [ds username] ;; TODO: it'd probably be better to use sql/find-by-key.
+  (let [ret (sql/query ds
+                       ["select id, email, username from accounts where username = ?"
+                        username])]
+    (when (seq ret)
+      (first ret))))
+
+(defn make-token
+  "Takes a user UUID and returns a token map
+  suitable for insertion in database."
+  [owner-id]
+  {:token      (-> (java.util.UUID/randomUUID) (.toString))
+   :owner      owner-id
+   :created_at (System/currentTimeMillis)})
+
+(defn insert-token! [datasource token]
+  (timelog-stdin "inserting token" token)
+  (sql/insert! datasource :pwtokens token))
+
+(defn forgot-pw!
+  "Takes a user map, generates a token, sends the e-mail containing
+  the password reset link."
+  [user]
+  (let [id      (:accounts/id user)
+        tok     (make-token id)
+        tok-str (:token tok)
+        email   (:accounts/email user)]
+    (insert-token! ds tok)
+    (send-forgot-email! email username tok-str)))
+
+;; TODO: check error pre conditions, verify token, reset password.
